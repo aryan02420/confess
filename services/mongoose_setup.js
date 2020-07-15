4b436a51789f89abe97593db0e1c8246ad247114
Aryan <aryan@pop-os.localdomain>
2020-07-15 16:28:36 +0530

    fixed dotenv

--- services/mongoose_setup.js
+++ services/mongoose_setup.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+if (!(process.env.NODE_ENV === "production")) {
+    require('dotenv').config();
+}
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.mongo_dbURI || 'mongodb://localhost/bpgc', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
